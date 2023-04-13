3e90e673da2dd9f795f22c58c06cb0752277278c
SeminKang <ksm9802@gmail.com>
2023-04-14 06:10:27 +0900

    Refactor: 필요없는 콘솔 삭제 #34
    try catch에서 오류 콘솔에 출력하는 거 빼고 다 삭제

--- src/context/hooks/useAuth.jsx
+++ src/context/hooks/useAuth.jsx
@@ -6,7 +6,6 @@ export default function useAuth() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
-        console.log("storedValue 변함 -> setIsLoggedIn 트리거");
         setIsLoggedIn(storedValue);
     }, [storedValue]);
 
